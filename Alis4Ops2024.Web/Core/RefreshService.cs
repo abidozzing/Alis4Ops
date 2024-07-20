98682bbbc6a967268396d1d2f79255f74dda804d
Abdullah Aljunied <abaljunied@gmail.com>
2024-07-21 00:48:46 +0800

    Shift "Good Job !", "Please Try Again" on same line as first operand vertical operation. Adjusted colour span for divide symbol.

--- Alis4Ops2024.Web/Core/RefreshService.cs
+++ Alis4Ops2024.Web/Core/RefreshService.cs
@@ -7,16 +7,22 @@ using Microsoft.JSInterop;
 
 namespace Alis4Ops2024.Web.Core
 {
-    // RefreshService.cs
     using System;
 
     public class RefreshService
     {
-        public event Action RefreshRequested;
+        public event Action? RefreshRequested;
 
         public void RequestRefresh()
+        {
+            // Invoke RefreshRequested event immediately when RequestRefresh() is called
+            OnRefreshRequested();
+        }
+
+        private void OnRefreshRequested()
         {
             RefreshRequested?.Invoke();
         }
     }
 }
+
